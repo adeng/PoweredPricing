e4b67ba46cbfda43bcca5f3a9d344b61f18ca0f6
Albert Deng <essential.app.dev@gmail.com>
2022-05-15 11:47:07 -0700

    Complete pricing page mockup

--- src/app/pages/project/pricing-page/pricing-page.component.ts
+++ src/app/pages/project/pricing-page/pricing-page.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pricing-page',
-  templateUrl: './pricing-page.component.html',
-  styleUrls: ['./pricing-page.component.scss']
+    selector: 'app-pricing-page',
+    templateUrl: './pricing-page.component.html',
+    styleUrls: ['./pricing-page.component.scss']
 })
 export class PricingPageComponent implements OnInit {
 
-  constructor() { }
+    PRICING_DATA: Array<Object> = [
+        {
+            process: "Functional",
+            fees: 200000,
+            margin: 0.4,
+            erp: 0.2,
+            rph: 250
+        },
+        {
+            process: "Technical",
+            fees: 200000,
+            margin: 0.42,
+            erp: 0.21,
+            rph: 263
+        },
+        {
+            process: "Governance",
+            fees: 200000,
+            margin: 0.43,
+            erp: 0.2,
+            rph: 267
+        }
+    ];
 
-  ngOnInit() {
-  }
+    columns: Array<string> = ["process", "fees", "margin", "erp", "rph"]
+    displayColumns: Object = {
+        process: "Process Area",
+        fees: "Billable Fees",
+        margin: "Margin %",
+        erp: "ERP %",
+        rph: "Average RPH"
+    }
 
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    calculateTotalFees(): number {
+        let total = 0;
+
+        for(let i = 0; i < this.PRICING_DATA.length; i++) {
+            total += this.PRICING_DATA[i]["fees"];
+        }
+
+        return total;
+    }
+
+    
+    calculateAverageMetric(metric: string): number {
+        let total = 0;
+
+        for(let i = 0; i < this.PRICING_DATA.length; i++) {
+            total += this.PRICING_DATA[i][metric];
+        }
+
+        return total / this.PRICING_DATA.length;
+    }
 }
--- src/app/pages/project/pricing-page/pricing-page.module.ts
+++ src/app/pages/project/pricing-page/pricing-page.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
 import { PricingPageComponent } from "./pricing-page.component";
+
+import { MatTableModule } from '@angular/material';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule
+        FormsModule,
+        MatTableModule,
+        MatSlideToggleModule,
+        MatInputModule,
+        MatFormFieldModule
     ],
     declarations: [PricingPageComponent]
 })
